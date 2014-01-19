92b9639288de9e84aa5482fa7a011c471d996b13
MadCat34 <madcat34@gmail.com>
2014-01-19 17:17:15 +0100

    Init project

---
+++ config/autoload/database.local.php
@@ -0,0 +1,28 @@
+<?php
+$dbParams = array(
+    'database'  => 'jobeet',
+    'username'  => 'jobeet',
+    'password'  => 'jobeet',
+    'hostname'  => 'localhost'
+);
+
+return array(
+    'service_manager' => array(
+        'factories' => array(
+            'Zend\Db\Adapter\Adapter' => function ($sm) use ($dbParams) {
+                $adapter = new BjyProfiler\Db\Adapter\ProfilingAdapter(array(
+                    'driver'    => 'pdo',
+                    'dsn'       => 'mysql:dbname='.$dbParams['database'].';host='.$dbParams['hostname'],
+                    'database'  => $dbParams['database'],
+                    'username'  => $dbParams['username'],
+                    'password'  => $dbParams['password'],
+                    'hostname'  => $dbParams['hostname'],
+                ));
+
+                $adapter->setProfiler(new BjyProfiler\Db\Profiler\Profiler);
+                $adapter->injectProfilingStatementPrototype();
+                return $adapter;
+            },
+        ),
+    ),
+);
---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+define('REQUEST_MICROTIME', microtime(true));
+
+
+// Setup autoloading
+require 'init_autoloader.php';
+
+// Run the application!
+Zend\Mvc\Application::init(require 'config/application.config.php')->run();
